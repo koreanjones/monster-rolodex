db557a9e27e0bd3e152ff906ae96a2f531c1c7b5
koreanjones <charles.justin.jones@gmail.com>
2020-06-16 13:24:12 -0700

    Updates

---
+++ precache-manifest.8efec69635d545c454bbe49f4fa41cff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4827cca96cc93735a7d252a669dd5657",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5e47831715853b4fc2f6",
+    "url": "/monster-rolodex/static/css/main.72062d9c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e47831715853b4fc2f6",
+    "url": "/monster-rolodex/static/js/main.2adcf337.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
